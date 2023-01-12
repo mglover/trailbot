f9ac1ee797a654809abc75b3f67aaf7cb1ac36ac
MG <shoemaker@riseup.net>
2023-01-12 14:31:20 -0800

    pricing interface rewrite

---
+++ dbhtml.py
@@ -0,0 +1,121 @@
+class Form(object):
+    row_idx = 0
+    @classmethod
+    def nextidx(cls):
+        i = cls.row_idx
+        cls.row_idx += 1
+        return i
+
+    @classmethod
+    def cellname(cls, colnum):
+        return "%i,%i" % (cls.row_idx, colnum)
+
+    @classmethod
+    def begin(cls, table, action="replace"):
+        cls.row_idx=0
+        r = []
+        r.append('<form method="POST">')
+        r.append('<input type="hidden" name="db" value="%s">'%table.dbnam)
+        r.append('<input type="hidden" name="action" value="%s">'%action)
+        return '\n'.join(r)
+
+    @classmethod
+    def end(cls):
+        return "</form>"
+
+    @classmethod
+    def select(cls, row, idx):
+        nam = cls.cellname(idx)
+        val = row.data[idx]
+        if 'options' in dir(val):
+            h = '<select name="%s">' % nam
+            for o in val.options():
+                selected = str(o)==str(val) and 'selected' or ''
+                h+= '\n\t<option value="%s" %s>%s</option>'%(o,selected,o)
+            h += '</select>'
+        return h
+
+    @classmethod
+    def _input(cls, row, idx, type="text"):
+        nam=cls.cellname(idx)
+        val=row.data[idx]
+        return '<input type="%s" name="%s" value="%s">' % (type, nam,val)
+
+    @classmethod
+    def edit(cls, row, idx):
+        nam = cls.cellname(idx)
+        val = row.data[idx]
+        if 'options' in dir(val):
+             return cls.select(row, idx)
+        else:
+             return cls._input(row, idx)
+
+
+    @classmethod
+    def text(cls, row,idx):
+            return str(row.data[idx])
+
+    @classmethod
+    def row_inputs(cls, row, cols):
+        r = []
+        for colnum in range(len(cols)):
+            c = cols[colnum]
+            if 'hidden' in c.tags:
+                r.append(cls._input(row, colnum, type="hidden"))
+            elif 'edit' in c.tags:
+                r.append(cls.edit(row, colnum))
+            elif 'computed' in c.tags:
+                r.append(cls.text(row, colnum))
+            else:
+                r.append(cls._input(row, colnum, type="hidden") \
+                    +cls.text(row,colnum))
+        return r
+
+    @classmethod
+    def tr(cls, row,cols):
+        cls.nextidx()
+        r = []
+        r.append("<tr>")
+        cells = cls.row_inputs(row, cols)
+        for c,v in zip(cols, cells):
+            td = 'hidden' not in c.tags
+            if td:
+                htcls=str(c.typ.__name__)
+                r.append('<td class="%s">'%htcls)
+            r.append(v)
+            if td:
+                r.append("</td>")
+
+
+        r.append("</tr>")
+        return '\n'.join(r)
+
+    @classmethod
+    def thead(cls, cols):
+        r = []
+        r.append("<thead><tr>")
+        for c in cols:
+            if 'hidden' not in c.tags:
+                htcls=str(c.typ.__name__)
+                r.append('<th class="%s">%s</th>'%(htcls, c.name))
+        r.append("</tr></thead>")
+        return '\n'.join(r)
+
+    @classmethod
+    def tfoot(cls, cols):
+        return "<tfoot></tfoot>"
+
+    @classmethod
+    def table(cls, rows, cols):
+        r = []
+        r.append('<table class="db">')
+        r.append(cls.thead(cols))
+        for row in rows:
+            r.append(cls.tr(row, cols))
+        r.append(cls.tfoot(cols))
+        r.append("</table>")
+        return '\n'.join(r)
+
+    @classmethod
+    def submit(cls, label):
+        return '<input type=submit value="%s">'%label
--- pricing.py
+++ pricing.py
@@ -1,53 +1,21 @@
-from flask import request, redirect, render_template,url_for
+from flask import request, redirect, render_template,url_for, Blueprint
 from db import Column, Component, Product, usd
+from dbhtml import Form
 
-def updateFromPostData(postdata):
-    return
-
-
-def html_select(row, idx):
-    nam = "%i,%i"%(row.idx,idx)
-    val = row.data[idx]
-    if 'options' in dir(row.data[idx]):
-        h = '<select name="%s">' % nam
-        for v in val.options():
-            selected = v==val and 'selected' or ''
-            h+= '\n\t<option value="%s" %s>%s</option>'%(v,selected,v)
-        h += '</select>'
-    return h
-
-def html_input(row, idx, type="text"):
-    nam="%i,%i"%(row.idx,idx)
-    val=row.data[idx]
-    return '<input type="%s" name="%s" value="%s">' % (type, nam,val)
-
-def html_edit(row, idx, **kwargs):
-    nam = "%i,%i"%(row.idx,idx)
-    val = row.data[idx]
-    if 'options' in dir(row.data[idx]):
-         return html_select(row, idx)
-    else:
-         return html_input(row, idx)
-
-def html_hidden(row, idx):
-    return  html_input(row, idx, type="hidden")
-
-def html_text(row,idx):
-        return str(row.data[idx])
-
+bp = Blueprint('pricing', __name__, url_prefix='/pricing')
 
 class View(object):
     vcols = []
     def __init__(self, table, rows=None):
         self.table = table
-#        print([c.name for c in self.table.cols+self.vcols])
         self.cols = self.table.cols + self.vcols
-        self.rows = rows or []
-#        for r in self.rows:
-#            print(r.data)
+        self.rows = []
+        if rows:
+            for row in rows: self.append(row)
+
     @classmethod
     def fromTable(cls, table):
-        rows = [cls.compute(r) for r in table.select()]
+        rows = table.select()
         return cls(table, rows)
 
     @classmethod
@@ -55,48 +23,9 @@ class View(object):
         return row
 
     def append(self, row):
-        self.rows.append(self.compute(row))
-
-    def forminputs(self, row):
-        r = []
-        for colnum in range(len(self.cols)):
-            c = self.cols[colnum]
-            if 'hidden' in c.tags:
-                r.append(html_hidden(row, colnum))
-            elif 'edit' in c.tags:
-                print('EDIT', row, colnum)
-                r.append(html_edit(row, colnum))
-            elif 'computed' in c.tags:
-                print('COMPUTED', row, colnum)
-                r.append(html_text(row, colnum))
-            else:
-                print('DEFAULT', row, colnum)
-                r.append(html_hidden(row, colnum)+
-                    html_text(row,colnum))
-        return r
-
-    def tHeader(self):
-        r = []
-        r.append("<tr>")
-        for c in self.cols:
-            if 'hidden' not in c.tags:
-                r.append("<td>"+c.name+"</td>")
-        r.append("</tr>")
-        return '\n'.join(r)
-
-    def formTable(self):
-        r = []
-        r.append('<table class="db">')
-        r.append(self.tHeader())
-        for row in self.rows:
-            r.append("<tr>")
-            cells = self.forminputs(row)
-            for c,v in zip(self.cols, cells):
-                if 'hidden' not in c.tags:
-                    r.append("<td>"+v+"</td>")
-            r.append("</tr>")
-        r.append("</table>")
-        return '\n'.join(r)
+        newrow = self.compute(row)
+        newrow.idx = len(self.rows)
+        self.rows.append(self.compute(newrow))
 
     def groupByIndex(self, idx):
         groups = {}
@@ -119,30 +48,43 @@ class PricingView(View):
     def compute(cls, row):
         pnam,compo,cnt = row.data[:3]
         ea = compo.price()
-        subt = ea * cnt
+        subt = usd(ea * cnt)
         row.data = (pnam,compo,cnt,ea,subt)
         return row
 
     def total(self):
         return sum([x.data[4] for x in self.rows])
 
-
+@bp.route('/', methods=('GET','POST'))
 def index():
     Component.load()
     Product.load()
 
     if request.method == 'POST':
-        updateFromPostData(request.form)
+        Component.updateFromPostData(request.form)
+        Product.updateFromPostData(request.form)
 
     return render_template('pricing.html',
-        components=View.fromTable(Component), 
+        form=Form,
+        components=View.fromTable(Component),
         pricing=PricingView.fromTable(Product))
 
-
+@bp.route('/new', methods=('GET', 'POST'))
 def new():
     Component.load()
     Product.load()
 
-    product = View(Product, [Product.newrow()])
+    if request.method == 'POST':
+        Product.updateFromPostData(request.form)
+        Product.save()
+        redirect(url_for('pricing.index'))
+    pnam = request.args.get('pnam')
+
+    product = View(Product, [Product(data={"Product":pnam})]*6)
+    for c in product.cols:
+        c.tags.append('edit')
+
     return render_template('newproduct.html',
+        form=Form,
+        pnam=pnam,
         product=product)
