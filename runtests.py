7cb02ae43c244dd3ba66f03d49084b88a04d6082
MG <shoemaker@riseup.net>
2024-02-19 14:14:39 -0700

    runtests: supress TLS warnings; w args, run only named tests

--- runtests.py
+++ runtests.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-import sys, os
+import sys, os, warnings
 import unittest
+from urllib3.connectionpool import InsecureRequestWarning
 
 tbroot = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(tbroot)
 
+testbase = 'trailbot.tests.test_'
+
+# requests run through the local proxy with an unknown cert
+warnings.simplefilter(
+    "ignore", 
+    category=InsecureRequestWarning,
+    lineno=1045
+)
 
 if __name__ == '__main__':
-    tests = unittest.TestLoader().discover(tbroot)
+    specs = sys.argv[1:]
+    loader = unittest.TestLoader()
+
+    if not specs:
+        tests = loader.discover(tbroot)
+    else:
+        tests = loader.loadTestsFromNames([testbase+s for s in specs])
+
     runner = unittest.TextTestRunner(sys.stdout, verbosity=1)
     runner.run(tests)
