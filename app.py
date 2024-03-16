0c0cf5c6fa8daeeda9caa844c8839f8b71d394c4
MG <mglover@pobox.com>
2024-03-16 13:55:45 -0600

    pricing,content and style updates

--- app.py
+++ app.py
@@ -8,14 +8,6 @@ import smswx, config
 app = Flask(__name__)
 app.TESTING=True
 
-
-@app.context_processor
-def inject_static():
-    def static(f):
-        return url_for('static', filename=f)
-    return dict(static=static)
-
-
 class BasicView(View):
     def __init__(self, template):
         self.template=template+'.html'
@@ -23,18 +15,72 @@ class BasicView(View):
         return render_template(self.template)
 
 
+
 app.add_url_rule('/', view_func=BasicView.as_view('/', 'index'))
 app.add_url_rule('/wx', view_func=smswx.sms_reply)
 
-for r in ['molding', 'turnshoes', 'welted', 'smswx', 'secret_recipe']:
+for r in ['molding', 'smswx', 'secret_recipe','location']:
     app.add_url_rule('/'+r, view_func=BasicView.as_view(r, r))
+
+def getdb(dbnam):
+    p = os.path.join(config.DB_ROOT, dbnam+'.csv')
+    fd = open(p)
+    return csv.reader(fd)
+
+def mkprice(svcs):
+    pricing = getdb('shoeprice')
+    t=0
+    for n,p in pricing:
+         if n in svcs: t = t+int(p)
+    return t
+
+
+turn = [
+    "Turnshoe Construction",
+    "Latigo Insole", 
+    "Stitching", "Patternmaking", "Uppers Leather"
+]
+
+welt= [
+    "Welted Construction",
+    "Vegtan Insole", "Vibram Outsole",
+    "Stitching", "Patternmaking", "Uppers Leather"
+]
+
+derby = ["Craft"]
+oxford = ["Craft 2"]
+ankle = ["Ankle Boot"]
+vegtan = ["Waxed Vegtan"]
+heel = ["Heel Stiffener"]
+eva = ["EVA Midsole"]
+cork = ["Cork Midsole"]
 
 
 @app.route('/goods')
 def goods():
-    db = open(os.path.join(config.DB_ROOT, "goods.csv"))
-    products = csv.reader(db)
+    products = getdb('goods')
     return render_template('goods.html', products=products)
+
+@app.route('/welted')
+def welted():
+    entry=welt+vegtan+ankle+derby+cork+heel
+    products=[
+        ('',mkprice(entry),'Welted derby ankle boot','before.jpg')
+    ]
+    return render_template('welted.html', title='Hiking Boots',
+        products=products)
+
+@app.route('/turnshoes')
+def turnshoes():
+    entry=turn
+    products = [
+        ('',mkprice(entry),'Low-cut front seam','turnshoes_jpg.jpg'),
+        ("",mkprice(entry+ankle),'Front seam ankle boot','trailshoe.jpg'),
+        ('',mkprice(entry+derby),'Low-cut derby','turnshoes_liz.jpg'),
+        ]
+    return render_template('turnshoes.html', title='Turnshoes',
+        products=products)
+
 
 if __name__ == '__main__':
     app.run(debug=False)
