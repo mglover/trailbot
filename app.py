72a021a11b0092c15a8f1c1b7517a61eaa3870bd
MG <mglover@pobox.com>
2024-03-16 13:57:00 -0600

    Galleries: recursive, hidden; add welted, turnshoe gals

--- app.py
+++ app.py
@@ -60,34 +60,55 @@ def news():
     return render_template('news.html', articles=articles)
 
 
-def getGallery(gname):
+def getData(gname):
     try:
         gfile = os.path.join(config.DB_ROOT, 'galleries', gname)
-        return json.load(open(gfile))
+        gdata = json.load(open(gfile))
     except FileNotFoundError:
         abort(404)
+    return gdata
+
+def getPreview(gname):
+    try:
+        gdata = getData(gname)
+    except  json.JSONDecodeError:
+        return None
+    if gdata.get('hidden', False):
+        return  None
+    first_photo = gdata['items'][0][0]
+    caption = gdata['title']
+    link = url_for('gallery', gname=gname)
+    return (first_photo,caption, link)
+
 
 @app.route('/gallery/<gname>')
 def gallery(gname):
-    data = getGallery(gname)
-    return render_template('gallery.html', gallery=data['items'],
-        title=data['title'])
+    gdata = getData(gname)
+    title = gdata['title']
+    gallery = []
+    for item in gdata['items']:
+        if len(item) < 2:
+            # this is a link to another gallery
+            p = getPreview(item[0])
+            if p:
+                gallery.append(p)
+        else:
+            gallery.append((item[0], item[1], ''))
+
+    return render_template('gallery.html', gallery=gallery,
+        title=gdata['title'])
+
 
 @app.route('/gallery')
-def metagallery():
+def gallery_list():
     gdir = os.path.join(config.DB_ROOT, 'galleries')
     gallery = []
     for g in os.listdir(gdir):
-        try:
-            data = getGallery(g)
-        except  json.JSONDecodeError:
-            continue
-        first_photo = data['items'][0][0]
-        caption = data['title']
-        link = url_for('gallery', gname=g)
-        gallery.append((first_photo,caption, link))
+        p = getPreview(g)
+        if p : gallery.append(p)
+
     return render_template('gallery.html', gallery=gallery,
-        title="Photo Galleries")
+        title="Photo Galleries", click="link")
 
 @app.route('/goods')
 def goods():
