a0fe85b76a836aae36e335c88da946ed98878f10
MG <mglover@pobox.com>
2024-04-14 08:07:39 -0600

    bot: locking, fix bot-phone check

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import requests
+import os, requests, time
 
 from . import config
 from .user import User, HandleUnknownError
@@ -10,18 +10,43 @@ baseurl_sending = "https://api.twilio.com/2010-04-01/Accounts/%s/Messages.json" 
 class Bot(object):
     handle = None
     phone = None
+    sleep = 30
 
     def _isBotPhone(self, phone):
         assert type(phone) is str
-        return self.phone.startswith("+07807")
+        return phone.startswith("+07807")
+
+    def _obtainLock(self):
+        gofile = os.path.join(self.user.dbpath, self.user.userdir, 'bot')
+        done = False
+        while not done:
+            try:
+                os.unlink(gofile)
+                done = True
+            except FileNotFoundError:
+                print("Waiting on %s" % gofile)
+                time.sleep(self.sleep)
+                pass
+
+    def _releaseLock(self):
+        gofile = os.path.join(self.user.dbpath, self.user.userdir, 'bot')
+        open(gofile, 'w').write('go')
 
     def __init__(self):
         assert self._isBotPhone(self.phone)
         try:
             self.user = User.lookup(self.handle)
             assert self.phone == self.user.phone
+
         except HandleUnknownError:
             self.user = User.register(self.phone, self.handle)
+
+        self._obtainLock()
+
+
+    def __del__(self):
+        self._releaseLock()
+
 
     def send_to_phone(self, phone, msg):
         if phone == self.phone: return
