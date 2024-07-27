785dcec71e8cc4283d4a62aa976ff311b55003aa
MG <mglover@pobox.com>
2024-07-27 06:47:17 -0600

    fix location test for output change

--- tests/test_location.py
+++ tests/test_location.py
@@ -4,13 +4,13 @@ class TestWhere(TBTest):
     def test_where_nom(self):
         self.reg1()
         res = self.req1("where Empire State Building")
-        self.assertStartsWith(res, "Empire State Building")
+        self.assertStartsWith(res, '"Empire State Building')
 
     def test_where_citystate(self):
         res = self.req1("where portland, or")
-        self.assertStartsWith(res, "portland, or")
+        self.assertStartsWith(res, '"portland, or')
 
     def test_where_zip(self):
         res = self.req1("where 97214")
-        self.assertStartsWith(res, "97214")
+        self.assertStartsWith(res, '"97214')
 
