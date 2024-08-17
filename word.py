5ef06cefc857e942c44d5f7c4a0fd138dfe7d9c7
MG <mglover@pobox.com>
2024-08-17 08:23:24 -0600

    word: refactor

--- word.py
+++ word.py
@@ -9,6 +9,15 @@ from .dispatch import tbroute, tbhelp
 
 from .db.TWL06 import twl as twl_mod
 
+def randomWord():
+    word = ''
+    next = ''
+    while next != '$':
+        word += next
+        next = random.choice(twl_mod.children(word))
+    return word
+
+
 class DictionarySource (NetSource):
     # dictionary and thesaurus access
     # via Merriam-Webster dictionaryapi.com
@@ -40,14 +49,6 @@ class DictionarySource (NetSource):
             )
 
 def tournamentWordOfTheDay():
-    def randomWord():
-        word = ''
-        next = ''
-        while next != '$':
-            word += next
-            next = random.choice(twl_mod.children(word))
-        return word
-
     dbroot = os.path.join(config.DB_ROOT, 'twotd')
     today = datetime.date.today()
     dbfile = os.path.join(dbroot, today.strftime("%Y%m%d"))
@@ -114,7 +115,7 @@ def twl(req):
         return f"NO. '{w}' is not a valid word"
 
 
-@tbroute('twotd')
+@tbroute('twotd', cat='games')
 @tbhelp(
 """Tournament Word of the Day
 a 2-5 letter word from the tournament word list
