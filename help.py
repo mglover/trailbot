02fd95e5bef469a8a1f971b2a99113e7c2823412
MG <mglover@pobox.com>
2024-08-18 14:27:31 -0600

    help: flatten logic

--- help.py
+++ help.py
@@ -1,20 +1,23 @@
 from flask import render_template
-from .dispatch import routes, tbroute, getAction, UnknownAction
+from .dispatch import routes, cats, tbroute, getAction, UnknownAction
 
 @tbroute('help')
 def help(req):
-    if req.args:
-        hcmd = req.args
-        if hcmd in ('me', 'all', '*'):
-            all = [spec for spec,cmd in routes if hasattr(cmd, '_help')]
-            return render_template("help_all.txt", all=all)
-        hfxn = getAction(hcmd)
-        if not hfxn: raise UnknownAction(hcmd)
-        if hasattr(hfxn, '_help'):
-            return hfxn._help
-        else:
-            return f"Sorry, I don't know anything else about {hcmd}"
-    else:
+    if not req.args:
         ## This case is handled by Twilio upstream
-        msg = render_template('help.txt')
-    return msg
+        return  render_template('help.txt')
+    hcmd = req.args
+
+    if hcmd in ('me', 'all', '*'):
+        all = [spec for spec,cmd in routes if hasattr(cmd, '_help')]
+        return render_template("help_all.txt", all=all)
+
+    hfxn = getAction(hcmd)
+    if not hfxn:
+        raise UnknownAction(hcmd)
+
+    if hasattr(hfxn, '_help'):
+        return hfxn._help
+    else:
+        return f"Sorry, I don't know anything else about {hcmd}"
+
